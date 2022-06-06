27f6bd2539ef6c89a09144dcdbe9b5ab6c126bab
Bree' Frost <101161806+BMikelFrost@users.noreply.github.com>
2022-06-06 11:53:54 -0700

    Add files via upload

---
+++ First.java/First.java
@@ -0,0 +1,10 @@
+import javax.swing.plaf.synth.SynthEditorPaneUI;
+
+public class First {
+    public static void main(String[] args){
+        System.out.println("My name is Bree'");
+        System.out.println("I am 100 years old");
+        System.out.println("Home Town is Spokane Valley, Washington");
+    }
+
+}
