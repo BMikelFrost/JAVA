5b17efac96354fd965cc37cf208dad40e135b1a9
Bree' Frost <101161806+BMikelFrost@users.noreply.github.com>
2022-06-09 10:23:28 -0700

    Add files via upload

---
+++ BankAccount/TestBankAccount.java
@@ -0,0 +1,16 @@
+
+
+public class TestBankAccount {
+
+	public static void main(String[] args) {
+		
+        BankAccount account = new BankAccount();
+        
+        account.deposit("checkingBalance", 100);
+        account.displayBalances();
+        account.getCheckingBalance();
+        System.out.println(account.withdraw("checkingBalance", 100));
+		System.out.println(account.withdraw("savingsBalance", 700 ));
+    
+    }	
+}
